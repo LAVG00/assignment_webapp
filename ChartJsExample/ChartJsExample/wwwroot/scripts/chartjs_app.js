dbe4a488f37cdee5f1be4440941f3e585e8ce480
LAVG00 <lavg8759@gmail.com>
2023-06-15 23:53:26 -0600

    Assignment 1 - Task 1

--- ChartJsExample/ChartJsExample/wwwroot/scripts/chartjs_app.js
+++ ChartJsExample/ChartJsExample/wwwroot/scripts/chartjs_app.js
@@ -1,30 +1,70 @@
-export async function draw() {
+
+export async function draw(something) {
+    try {
+        console.log(something)
+        const response = await fetch('../sample-data/reports.json');
+        const reports = await response.json();
+
+        // const datesArray = reports.map(report => report.WorkDate.split('T')[0]);
+
+        // const maintenanceTimeArray = reports.map(report => report.WorkTime.Maintenance )
+
+        // console.log(reports) 
+        // console.log(datesArray)
+        // console.log(maintenanceTimeArray)
+
+        // const maintenanceTimePerDay = reports.reduce((result, report) => {
+        //     const date = new Date(report.WorkDate).toLocaleDateString('ja-JP');
+        //     const formattedDate = date.replace(/\//g, '-');
+        //     const maintenanceTime = report.WorkTime.Maintenance;
+
+        //     if (result[formattedDate]) {
+        //         result[formattedDate] += maintenanceTime;
+        //       } else {
+        //         result[formattedDate] = maintenanceTime;
+        //       }
+        //       return result;
+        // }, {});
+
+        const mostRecentDay = new Date(Math.max(...reports.map(report => new Date(report.WorkDate))));
+        const lastSevenDays = [];
+        const maintenanceTimePerDay = {};
+
+        //Obtain last 7 days from the most recent date
+        for(let i = 6; i >= 0 ; i--){
+            const date = new Date(mostRecentDay);
+            date.setDate(date.getDate() - i);
+            lastSevenDays.unshift(date.toLocaleDateString('ja-JP').replace(/\//g, '-'));
+        }
+        //initialize maintenanceTimePerDay
+        lastSevenDays.forEach(date => { maintenanceTimePerDay[date] = 0});
+        //Assign maintenance time to date
+        reports.forEach(report => {
+            const workDate = new Date(report.WorkDate).toLocaleDateString('ja-JP').replace(/\//g, '-');
+            const maintenanceTime = report.WorkTime.Maintenance;
+            if(maintenanceTimePerDay.hasOwnProperty(workDate)) {
+                maintenanceTimePerDay[workDate] += maintenanceTime;
+            }
+        })
+
+
+        console.log('mostRecentDay',mostRecentDay)
+        console.log(maintenanceTimePerDay);
+        const datesArray = Object.keys(maintenanceTimePerDay)
+        const maintenanceTimeArray = Object.values(maintenanceTimePerDay)
+
     // and just rendering very simple chart according to tutorial page: https://www.chartjs.org/docs/latest/getting-started/usage.html
     const ctx = document.getElementById('chartCanvas');
     window.myChart = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: datesArray,
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                label: 'Maintenance Time(Min):',
+                data: maintenanceTimeArray,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 3
             }]
         },
         options: {
@@ -35,4 +75,7 @@ export async function draw() {
             }
         }
     });
+} catch(e) {
+    console.log('Error', e)
+}
 };
